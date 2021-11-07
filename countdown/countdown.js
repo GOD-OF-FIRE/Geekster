6e674d872c28a3d99d2a21b04382af1818d8cb1a
Kushagra Gupta <kushagra357@gmail.com>
2021-11-07 14:59:08 +0530

    Add files via upload

---
+++ countdown/countdown.js
@@ -0,0 +1,18 @@
+// getting inner text of div which is default set 100
+let getTime = document.getElementById("countdown").innerText
+let time= document.getElementById("countdown")
+// convert string to integer for further use
+var integer=parseInt(getTime)
+// to call update function every seconds
+setInterval(update,1000)
+// function update the timer
+function update()
+{
+    integer -= 1;
+    time.innerText = integer;
+    if(integer<=0)
+    {
+        document.getElementById("countdown").innerHTML = "Timesup";
+        document.getElementById("container").style.backgroundColor="red";
+    }
+}
