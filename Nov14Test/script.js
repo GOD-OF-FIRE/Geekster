a82170e5e9628662e087e7b80b49c16a2c613180
Kushagra Gupta <kushagra357@gmail.com>
2021-11-14 15:15:15 +0530

    Add files via upload

---
+++ Nov14Test/script.js
@@ -0,0 +1,18 @@
+function fn1()
+{
+    var first = document.getElementById('first').value
+    document.getElementById('pf').innerText = first
+
+}
+function fn2()
+{
+    var second = document.getElementById('second').value
+    document.getElementById('ps').innerText = second
+
+}
+function fn3()
+{
+    var third = document.getElementById('third').value
+    document.getElementById('pt').innerText = third
+
+}
